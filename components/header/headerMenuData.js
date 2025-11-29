e9d14e3b82795608fcb1fef70633a2b73ff49dac
miladrs75 <miladrs7575@gmail.com>
2025-11-29 16:24:09 +0330

    add header menu data structure

---
+++ components/header/headerMenuData.js
@@ -0,0 +1,238 @@
+export const menuData = [
+  {
+    title: "Start Crypto Trading",
+    subtitle: [
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2641)">
+                <path d="M12.1984 9.07812C10.2864 9.07812 8.73438 10.6301 8.73438 12.5421C8.73438 14.4541 10.2864 16.0061 12.1984 16.0061C14.1104 16.0061 15.6624 14.4541 15.6624 12.5421C15.6624 10.6301 14.1104 9.07812 12.1984 9.07812ZM12.1984 14.0301L10.7104 12.5421L12.1984 11.0541L13.6864 12.5421L12.1984 14.0301Z" fill="#D7A81C"/>
+                <path d="M7.597 12.5336C7.597 9.98963 9.653 7.93363 12.197 7.93363V0.765625H1.125V15.2296H8.477C7.925 14.4696 7.597 13.5416 7.597 12.5336ZM3.389 3.35763H10.293V4.71763H3.389V3.35763ZM7.093 8.42963H3.389V7.06963H7.093V8.42963Z" fill="currentColor"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2641">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg> `,
+        info: "Futures",
+        subinfo: "Contracts settled in cryptocurrency",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.4 9.664L9.664 14.4H14.4V9.664Z" fill="#D7A81C"/>
+                <path d="M1.60156 6.33756L6.33756 1.60156H1.60156V6.33756Z" fill="#D7A81C"/>
+                <path d="M6.4025 5.71875C4.2825 5.71875 2.5625 7.43875 2.5625 9.55875C2.5625 11.6787 4.2825 13.3987 6.4025 13.3987C8.5225 13.3987 10.2425 11.6787 10.2425 9.55875C10.2425 7.43875 8.5225 5.71875 6.4025 5.71875ZM6.4025 11.2147L4.7465 9.55875L6.4025 7.90275L8.0585 9.55875L6.4025 11.2147Z" fill="currentColor"/>
+                <path d="M10.1564 1.39844C8.40438 1.39844 6.94038 2.57444 6.48438 4.18244C9.06038 4.35844 11.1324 6.31044 11.5004 8.83044C12.9564 8.28644 13.9964 6.89444 13.9964 5.24644C13.9964 3.12644 12.2764 1.40644 10.1564 1.40644V1.39844Z" fill="currentColor"/>
+               </svg>`,
+        info: "Spot",
+        subinfo: "Buy and sell by advanced tools",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Convert",
+        subinfo: "The easiest way to trade at all sizes ",
+      },
+    ],
+  },
+  {
+    title: "cryptocurrency Overview",
+    subtitle: [
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2669)">
+                <path d="M12.1984 9.07812C10.2864 9.07812 8.73438 10.6301 8.73438 12.5421C8.73438 14.4541 10.2864 16.0061 12.1984 16.0061C14.1104 16.0061 15.6624 14.4541 15.6624 12.5421C15.6624 10.6301 14.1104 9.07812 12.1984 9.07812ZM12.1984 14.0301L10.7104 12.5421L12.1984 11.0541L13.6864 12.5421L12.1984 14.0301Z" fill="#D7A81C"/>
+                <path d="M7.597 12.5336C7.597 9.98963 9.653 7.93363 12.197 7.93363V0.765625H1.125V15.2296H8.477C7.925 14.4696 7.597 13.5416 7.597 12.5336ZM3.389 3.35763H10.293V4.71763H3.389V3.35763ZM7.093 8.42963H3.389V7.06963H7.093V8.42963Z" fill="currentColor"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2669">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Bitcoin price",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.4 9.664L9.664 14.4H14.4V9.664Z" fill="#D7A81C"/>
+                <path d="M1.60156 6.33756L6.33756 1.60156H1.60156V6.33756Z" fill="#D7A81C"/>
+                <path d="M6.4025 5.71875C4.2825 5.71875 2.5625 7.43875 2.5625 9.55875C2.5625 11.6787 4.2825 13.3987 6.4025 13.3987C8.5225 13.3987 10.2425 11.6787 10.2425 9.55875C10.2425 7.43875 8.5225 5.71875 6.4025 5.71875ZM6.4025 11.2147L4.7465 9.55875L6.4025 7.90275L8.0585 9.55875L6.4025 11.2147Z" fill="currentColor"/>
+                <path d="M10.1564 1.39844C8.40438 1.39844 6.94038 2.57444 6.48438 4.18244C9.06038 4.35844 11.1324 6.31044 11.5004 8.83044C12.9564 8.28644 13.9964 6.89444 13.9964 5.24644C13.9964 3.12644 12.2764 1.40644 10.1564 1.40644V1.39844Z" fill="currentColor"/>
+               </svg>`,
+        info: "Altcoin price",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Meme price",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Top gainers/losers",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "New listings",
+      },
+    ],
+  },
+  {
+    title: "Buy Crypto",
+    subtitle: [
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2669)">
+                <path d="M12.1984 9.07812C10.2864 9.07812 8.73438 10.6301 8.73438 12.5421C8.73438 14.4541 10.2864 16.0061 12.1984 16.0061C14.1104 16.0061 15.6624 14.4541 15.6624 12.5421C15.6624 10.6301 14.1104 9.07812 12.1984 9.07812ZM12.1984 14.0301L10.7104 12.5421L12.1984 11.0541L13.6864 12.5421L12.1984 14.0301Z" fill="#D7A81C"/>
+                <path d="M7.597 12.5336C7.597 9.98963 9.653 7.93363 12.197 7.93363V0.765625H1.125V15.2296H8.477C7.925 14.4696 7.597 13.5416 7.597 12.5336ZM3.389 3.35763H10.293V4.71763H3.389V3.35763ZM7.093 8.42963H3.389V7.06963H7.093V8.42963Z" fill="currentColor"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2669">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Buy bitcoin",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.4 9.664L9.664 14.4H14.4V9.664Z" fill="#D7A81C"/>
+                <path d="M1.60156 6.33756L6.33756 1.60156H1.60156V6.33756Z" fill="#D7A81C"/>
+                <path d="M6.4025 5.71875C4.2825 5.71875 2.5625 7.43875 2.5625 9.55875C2.5625 11.6787 4.2825 13.3987 6.4025 13.3987C8.5225 13.3987 10.2425 11.6787 10.2425 9.55875C10.2425 7.43875 8.5225 5.71875 6.4025 5.71875ZM6.4025 11.2147L4.7465 9.55875L6.4025 7.90275L8.0585 9.55875L6.4025 11.2147Z" fill="currentColor"/>
+                <path d="M10.1564 1.39844C8.40438 1.39844 6.94038 2.57444 6.48438 4.18244C9.06038 4.35844 11.1324 6.31044 11.5004 8.83044C12.9564 8.28644 13.9964 6.89444 13.9964 5.24644C13.9964 3.12644 12.2764 1.40644 10.1564 1.40644V1.39844Z" fill="currentColor"/>
+               </svg>`,
+        info: "Buy etheruem",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Buy memecoins",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "buy ai tokens",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Sell crypto",
+      },
+    ],
+  },
+  
+  {
+    title: "Info",
+    subtitle: [
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2669)">
+                <path d="M12.1984 9.07812C10.2864 9.07812 8.73438 10.6301 8.73438 12.5421C8.73438 14.4541 10.2864 16.0061 12.1984 16.0061C14.1104 16.0061 15.6624 14.4541 15.6624 12.5421C15.6624 10.6301 14.1104 9.07812 12.1984 9.07812ZM12.1984 14.0301L10.7104 12.5421L12.1984 11.0541L13.6864 12.5421L12.1984 14.0301Z" fill="#D7A81C"/>
+                <path d="M7.597 12.5336C7.597 9.98963 9.653 7.93363 12.197 7.93363V0.765625H1.125V15.2296H8.477C7.925 14.4696 7.597 13.5416 7.597 12.5336ZM3.389 3.35763H10.293V4.71763H3.389V3.35763ZM7.093 8.42963H3.389V7.06963H7.093V8.42963Z" fill="currentColor"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2669">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "About us",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.4 9.664L9.664 14.4H14.4V9.664Z" fill="#D7A81C"/>
+                <path d="M1.60156 6.33756L6.33756 1.60156H1.60156V6.33756Z" fill="#D7A81C"/>
+                <path d="M6.4025 5.71875C4.2825 5.71875 2.5625 7.43875 2.5625 9.55875C2.5625 11.6787 4.2825 13.3987 6.4025 13.3987C8.5225 13.3987 10.2425 11.6787 10.2425 9.55875C10.2425 7.43875 8.5225 5.71875 6.4025 5.71875ZM6.4025 11.2147L4.7465 9.55875L6.4025 7.90275L8.0585 9.55875L6.4025 11.2147Z" fill="currentColor"/>
+                <path d="M10.1564 1.39844C8.40438 1.39844 6.94038 2.57444 6.48438 4.18244C9.06038 4.35844 11.1324 6.31044 11.5004 8.83044C12.9564 8.28644 13.9964 6.89444 13.9964 5.24644C13.9964 3.12644 12.2764 1.40644 10.1564 1.40644V1.39844Z" fill="currentColor"/>
+               </svg>`,
+        info: "fees",
+      },
+      {
+        icon: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2659)">
+                <path d="M15.44 8.33063L8 0.890625H11.328" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M0.5625 7.75781L8.0025 15.1978H4.6745" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"/>
+                <path d="M7.99644 5.14844C6.42044 5.14844 5.14844 6.42044 5.14844 7.99644C5.14844 9.57244 6.42044 10.8444 7.99644 10.8444C9.57244 10.8444 10.8444 9.57244 10.8444 7.99644C10.8444 6.42044 9.57244 5.14844 7.99644 5.14844ZM7.99644 9.22044L6.77244 7.99644L7.99644 6.77244L9.22044 7.99644L7.99644 9.22044Z" fill="#D7A81C"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2659">
+                <rect width="16" height="16" fill="white"/>
+                </clipPath>
+                </defs>
+               </svg>`,
+        info: "Security",
+      },
+    ],
+  },
+];
