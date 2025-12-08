208facd787d7f9f472a49772d00bfb2d1595ab40
miladrs75 <miladrs7575@gmail.com>
2025-12-08 14:55:31 +0330

    update main js file

--- components/main/main.js
+++ components/main/main.js
@@ -1,11 +1,26 @@
 import { activeTabs } from "./activePopularCoinsTabs.js";
-import {fetchCryptoData} from "./fetchCryptoData.js"
+import { fetchCrypto } from "./fetchCryptoData.js";
 
 const popularCoins = document.querySelectorAll(".popular-coins");
 const hotFutures = document.querySelector("#hot-futures--list > ul");
 const newlyAdded = document.querySelector("#newly-added--list > ul");
+const topVolum = document.querySelector("#top-volume--list > ul");
+const hotTokes = document.querySelector("#hot-tokes--list > ul");
+const topLosers = document.querySelector("#top-losers--list > ul");
+const topGainers = document.querySelector("#top-gainers--list > ul");
+const topLosersDesktop = document.querySelector("#top-losers--list--desktop > ul");
+const topGainersDesktop = document.querySelector("#top-gainers--list--desktop > ul");
 
 activeTabs(popularCoins);
-
-fetchCryptoData(hotFutures, 0);
-fetchCryptoData(newlyAdded, 5);
+fetchCrypto(hotFutures, 0, 5 , "popular");
+// setInterval(() => fetchCrypto(hotFutures, 0, 5), 30000);
+fetchCrypto(newlyAdded, 5, 5, "popular");
+// setInterval(() => fetchCrypto(newlyAdded, 5, 5), 30000);
+fetchCrypto(topVolum, 0, 11, "live");
+// setInterval(() => fetchCrypto(topVolum, 0, 8), 30000);
+fetchCrypto(hotTokes, 11, 11, "live");
+// setInterval(() => fetchCrypto(hotTokes, 8, 8), 30000);
+fetchCrypto(topLosers, 0, 3, "topGainers");
+fetchCrypto(topGainers, 42, 3, "topGainers");
+fetchCrypto(topLosersDesktop, 0, 3, "topGainers");
+fetchCrypto(topGainersDesktop, 42, 3, "topGainers");
