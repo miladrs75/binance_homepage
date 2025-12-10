029d12ca43d6b04cbefdb2bc8e304211546d2ba1
miladrs75 <miladrs7575@gmail.com>
2025-12-10 13:59:36 +0330

    add carousel scripts

--- components/main/main.js
+++ components/main/main.js
@@ -1,5 +1,7 @@
 import { activeTabs } from "./activePopularCoinsTabs.js";
 import { fetchCrypto } from "./fetchCryptoData.js";
+import { renderSlideImages } from "./carousel.js";
+import { updateCarouselIndicators } from "./carousel.js";
 
 const popularCoins = document.querySelectorAll(".popular-coins");
 const hotFutures = document.querySelector("#hot-futures--list > ul");
@@ -8,11 +10,18 @@ const topVolum = document.querySelector("#top-volume--list > ul");
 const hotTokes = document.querySelector("#hot-tokes--list > ul");
 const topLosers = document.querySelector("#top-losers--list > ul");
 const topGainers = document.querySelector("#top-gainers--list > ul");
-const topLosersDesktop = document.querySelector("#top-losers--list--desktop > ul");
-const topGainersDesktop = document.querySelector("#top-gainers--list--desktop > ul");
+const topLosersDesktop = document.querySelector(
+  "#top-losers--list--desktop > ul"
+);
+const topGainersDesktop = document.querySelector(
+  "#top-gainers--list--desktop > ul"
+);
+const slideImages = document.querySelectorAll(".carousel-slide--image");
+const carousel = document.querySelector(".advanced-tools--carousel");
+const indicators = document.querySelectorAll(".indicator");
 
 activeTabs(popularCoins);
-fetchCrypto(hotFutures, 0, 5 , "popular");
+fetchCrypto(hotFutures, 0, 5, "popular");
 // setInterval(() => fetchCrypto(hotFutures, 0, 5), 30000);
 fetchCrypto(newlyAdded, 5, 5, "popular");
 // setInterval(() => fetchCrypto(newlyAdded, 5, 5), 30000);
@@ -24,3 +33,6 @@ fetchCrypto(topLosers, 0, 3, "topGainers");
 fetchCrypto(topGainers, 42, 3, "topGainers");
 fetchCrypto(topLosersDesktop, 0, 3, "topGainers");
 fetchCrypto(topGainersDesktop, 42, 3, "topGainers");
+
+renderSlideImages(slideImages);
+updateCarouselIndicators(carousel, indicators);
