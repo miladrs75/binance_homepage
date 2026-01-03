a8754f5a32030dcd083238ef050cd4f04706f3bb
miladrs75 <miladrs7575@gmail.com>
2026-01-04 01:05:46 +0330

    update fetch scripts

--- components/main/main.js
+++ components/main/main.js
@@ -1,5 +1,5 @@
 import { activeTabs } from "./activePopularCoinsTabs.js";
-import { fetchCrypto } from "./fetchCryptoData.js";
+import { fetchCrypto, loadCoins } from "./fetchCryptoData.js";
 import { renderSlideImages } from "./carousel.js";
 import { updateCarouselIndicators } from "./carousel.js";
 import { renderAppDownloadImages } from "./appDownload.js";
@@ -25,28 +25,30 @@ const appDownloadImages = document.querySelectorAll(
 
 activeTabs(popularCoins);
 
-fetchCrypto(hotFutures, 0, 5, "popular");
-setInterval(() => fetchCrypto(hotFutures, 0, 5, "popular"), 30000);
-fetchCrypto(newlyAdded, 5, 5, "popular");
-setInterval(() => fetchCrypto(newlyAdded, 5, 5, "popular"), 30000);
-fetchCrypto(topVolum, 0, 11, "live");
-setInterval(() => fetchCrypto(topVolum, 0, 11, "live"), 30000);
-fetchCrypto(hotTokes, 11, 11, "live");
-setInterval(() => fetchCrypto(hotTokes, 11, 11, "live"), 30000);
-fetchCrypto(topLosers, 0, 3, "topGainers");
-setInterval(() => fetchCrypto(topLosers, 0, 3, "topGainers"), 30000);
-fetchCrypto(topGainers, 42, 3, "topGainers");
-setInterval(() => fetchCrypto(topGainers, 42, 3, "topGainers"), 30000);
-fetchCrypto(topLosersDesktop, 0, 3, "topGainers");
-setInterval(() => fetchCrypto(topLosersDesktop, 0, 3, "topGainers"), 30000);
-fetchCrypto(topGainersDesktop, 42, 3, "topGainers");
-setInterval(() => fetchCrypto(topGainersDesktop, 42, 3, "topGainers"), 30000);
+function renderAllCrypto() {
+  fetchCrypto(hotFutures, 0, 5, "popular");
+  fetchCrypto(newlyAdded, 5, 5, "popular");
+  fetchCrypto(topVolum, 0, 11, "live");
+  fetchCrypto(hotTokes, 11, 11, "live");
+  fetchCrypto(topLosers, 0, 3, "topGainers");
+  fetchCrypto(topGainers, 42, 3, "topGainers");
+  fetchCrypto(topLosersDesktop, 0, 3, "topGainers");
+  fetchCrypto(topGainersDesktop, 42, 3, "topGainers");
+}
+
+async function initCrypto() {
+  await loadCoins();
+  renderAllCrypto();
+}
+
+initCrypto();
+setInterval(initCrypto, 30000);
 
 renderSlideImages(slideImages);
+
 carousels.forEach((carouselSection) => {
   const carousel = carouselSection.querySelector(".carousel");
   const indicators = carouselSection.querySelectorAll(".indicator");
-
   updateCarouselIndicators(carousel, indicators);
 });
 
